e5d36e91eb4e1bd5943adc8454d5fbf19aa3353e
svengrav <sven@svengrav.de>
2022-07-13 17:31:22 +0200

    Exception naming updated

--- src/AppInsights/Exceptions/InvalidInstrumentationKeyException.cs
+++ src/AppInsights/Exceptions/InvalidInstrumentationKeyException.cs
@@ -7,9 +7,11 @@ namespace AppInsights.Exceptions
     /// </summary>
     public class InvalidInstrumentationKeyException : Exception
     {
-        public const int ERROR_CODE = 40100;
-
+#pragma warning disable IDE1006 // Naming Styles
         private const string KEY_IS_INVALID = "Instrumentation Key is missing or not correct. Set the instrumentation key as an environment variable or add it as a parameter when calling the command.";
+#pragma warning restore IDE1006 // Naming Styles
+
+        public const int ERROR_CODE = 40100;
 
         public InvalidInstrumentationKeyException() : base(KEY_IS_INVALID)
         {
--- src/AppInsights/Telemetry/TelemetryInstrumentationKey.cs
+++ src/AppInsights/Telemetry/TelemetryInstrumentationKey.cs
@@ -5,7 +5,9 @@ namespace AppInsights.Telemetry
 {
     internal class TelemetryInstrumentationKey
     {
+#pragma warning disable IDE1006 // Naming Styles
         private const string INSTRUMENTATION_KEY_VARIABLE = "AI_INSTRUMENTATION_KEY";
+#pragma warning restore IDE1006 // Naming Styles
 
         private readonly Guid _instrumentationKey;
 
